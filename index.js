f08915b488ce5b89e5aebba1951b4c2571a16bee
sakib <s.sarder232002@gmail.com>
2023-06-07 23:48:30 +0600

    user info set in database

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -9,9 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//  
-
-
+//
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1gttryf.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,30 +18,50 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    //Collections
+    const usersCollection = client
+      .db("craftopia")
+      .collection("usersCollection");
+
+    // Save User Info In DB
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateUser = {
+        $set: user.currentUser,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateUser,
+        options
+      );
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
 
-
-
-
 app.get("/", (req, res) => {
-    res.send("Craftopia is Running")
-})
+  res.send("Craftopia is Running");
+});
 
 app.listen(port, () => {
-    console.log(`Craftopia is running on Port : ${port}`);
-})
+  console.log(`Craftopia is running on Port : ${port}`);
+});
