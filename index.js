b02288518b5c00878ee4333a345a27732d73cb29
sakib <s.sarder232002@gmail.com>
2023-06-08 21:44:22 +0600

    vercel deploy done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -21,61 +21,60 @@ const client = new MongoClient(uri, {
   },
 });
 
-async function run() {
+//
+const dbConnect = async () => {
   try {
-    await client.connect();
-
-    //Collections
-    const usersCollection = client
-      .db("craftopia")
-      .collection("usersCollection");
-    const classCollection = client
-      .db("craftopia")
-      .collection("classCollection");
-
-    // Save User Info In DB
-    app.put("/users/:email", async (req, res) => {
-      const email = req.params.email;
-      const user = req.body;
-      const filter = { email: email };
-      const options = { upsert: true };
-      const updateUser = {
-        $set: user.currentUser,
-      };
-      const result = await usersCollection.updateOne(
-        filter,
-        updateUser,
-        options
-      );
-      res.send(result);
-    });
-
-    // Add Class in DB
-    app.post("/classes", async (req, res) => {
-      const { addedClass } = req.body;
-      const result = await classCollection.insertOne(addedClass);
-      res.send(result);
-    });
-
-    // Get Classes
-    app.get("/classes", async (req, res) => {
-      const result = await classCollection.find().toArray();
-      res.send(result);
-    });
-
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
+    client.connect();
+    console.log(" Database Connected Successfully✅ ");
+  } catch (error) {
+    console.log(error.name, error.message);
   }
-}
-run().catch(console.dir);
+};
+dbConnect();
+
+//Collections
+const usersCollection = client.db("craftopia").collection("usersCollection");
+const classCollection = client.db("craftopia").collection("classCollection");
 
 app.get("/", (req, res) => {
   res.send("Craftopia is Running");
 });
+
+// Save User Info In DB
+app.put("/users/:email", async (req, res) => {
+  const email = req.params.email;
+  const user = req.body;
+  const filter = { email: email };
+  const options = { upsert: true };
+  const updateUser = {
+    $set: user.currentUser,
+  };
+  const result = await usersCollection.updateOne(filter, updateUser, options);
+  res.send(result);
+});
+
+// Add Class in DB
+app.post("/classes", async (req, res) => {
+  const { addedClass } = req.body;
+  const result = await classCollection.insertOne(addedClass);
+  res.send(result);
+});
+
+// Get Classes
+app.get("/classes", async (req, res) => {
+  const result = await classCollection.find().toArray();
+  res.send(result);
+});
+
+// Get Classes for Instructor
+app.get("/classes/:email", async (req, res) => {
+  const email = req.params.email;
+  console.log(email);
+  const query = { instructorEmail: email };
+  const result = await classCollection.find(query).toArray();
+  res.send(result);
+});
+
 
 app.listen(port, () => {
   console.log(`Craftopia is running on Port : ${port}`);
